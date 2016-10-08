91abe5bdc5c6940163b9014ec9ad9996042f5643
Mihael Safaric <mihael.safaric@gmail.com>
2016-10-09 01:40:34 +0200

    Add brewlist

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function prompt {
+    read -p "🍕  Are you sure you wish to continue? y/[n] "
+    if [ "$REPLY" != "y" ]; then
+	exit
+    fi
+}
+
+dir=`PWD`
+
+echo "This will setup your system. Please be sure you read what the script does"
+echo "before running it."
+prompt
+
+if ! command -v brew >/dev/null 2>&1; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo "Brew installed!"
+else
+    echo "Brew already installed!"
+fi
+
+brew tap caskroom/cask
+brew tap homebrew/services
+
+xargs brew install < packages/brewlist
+echo "Installed packages"
+
+n latest
+echo "Node set to $(node -v)"
